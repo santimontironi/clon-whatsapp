c5c9a5550aaa26a8ec9e0a139975dfc804f1b930
santimontironi <santiimontironi@gmail.com>
2025-09-29 17:04:30 -0300

    loading agregado en ContactList

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,19 +1,33 @@
 import Contact from "../Contact/Contact"
 import { getAllContacts } from "../../services/contactService"
 import { Link } from "react-router"
+import { useState, useEffect } from "react"
 import './ContactList.css'
 
 const ContactList = () => {
 
-  const contacts = getAllContacts()
+  const [loading, setLoading] = useState(false)
+  const [contacts, setContacts] = useState([])
+
+  const allContacts = getAllContacts()
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setContacts(allContacts)
+      setLoading(false)
+    }, 1000)
+  },[])
 
   return (
     <div className="contactList">
-      {contacts.map((contact) => (
-        <Link to={`/contacto/` + contact.id} key={contact.id}>
-          <Contact photoProfile={contact.profile_photo} name={contact.name} last_time={contact.last_time_conneceted} isConnected={contact.is_connected ? 'En linea' : ''} state={contact.state}/>
-        </Link>
-      ))}
+      {loading ? 'loading...' :
+        contacts.map((contact) => (
+          <Link to={`/contacto/` + contact.id} key={contact.id}>
+            <Contact photoProfile={contact.profile_photo} name={contact.name} last_time={contact.last_time_conneceted} isConnected={contact.is_connected ? 'En linea' : ''} state={contact.state} />
+          </Link>
+        ))
+      }
     </div>
   )
 }
