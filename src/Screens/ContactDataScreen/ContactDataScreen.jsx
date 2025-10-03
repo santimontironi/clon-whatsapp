702a1cfdf498e1b22a2c308f932bf71ae441c627
santimontironi <santiimontironi@gmail.com>
2025-10-03 12:47:02 -0300

    estilos en ContactDataScreen.css

--- src/Screens/ContactDataScreen/ContactDataScreen.jsx
+++ src/Screens/ContactDataScreen/ContactDataScreen.jsx
@@ -1,6 +1,9 @@
 import { SyncLoader } from "react-spinners"
 import { useContext } from "react"
 import { ContactDetailContext } from "../../Context/ContactDetailContext"
+import { IoArrowBackOutline } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+import { Link } from "react-router";
 import './ContactDataScreen.css'
 
 const ContactDataScreen = () => {
@@ -10,15 +13,23 @@ const ContactDataScreen = () => {
 
     return (
         <div className="contactDataScreen">
-            {loading ? (
+            {loading || !contactDetail ?  (
                 <SyncLoader className="loader"/>
             ) : (
                 <div className="contactDataScreen-container">
-                    <h1 className="contactDataScreen-container-title">Info del contacto</h1>
+                    <header className="contactDataScreen-container-header">
+                        <Link to="/">
+                            <IoArrowBackOutline className="contactDataScreen-container-header-icon" color="#ffffff" size={30} />
+                        </Link>
+                        <h1 className="contactDataScreen-container-title">Info del contacto</h1>
+                    </header>
                     <div className="contactDataScreen-container-content">
-                        <img className="contactDataScreen-container-content-photo" src={contactDetail.profile_photo} alt={`Foto de perfil de ${contactDetail.name}`} />
-                        <span className="contactDataScreen-container-content-name">{contactDetail.name}</span>
-                        <span className="contactDataScreen-container-content-number">{contactDetail.number}</span>
+                        <figure className="contactDataScreen-container-content-photo">
+                            <img className="contactDataScreen-container-content-photo-img" src={contactDetail.profile_photo} alt={`Foto de perfil de ${contactDetail.name}`} />
+                        </figure>
+                        <span className="contactDataScreen-container-content-name">Contacto: {contactDetail.name}</span>
+                        <span className="contactDataScreen-container-content-number"><FaPhoneAlt color="#fff" size={20} /> {contactDetail.number}</span>
+                        <span className="contactDataScreen-container-content-state">Estado: {contactDetail.state}</span>
                     </div>
                 </div>
             )}
