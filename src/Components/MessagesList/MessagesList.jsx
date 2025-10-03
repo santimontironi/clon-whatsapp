f60d88eb3bf13aaa8814e192c7adcee24eb478e5
santimontironi <santiimontironi@gmail.com>
2025-10-03 14:45:16 -0300

    scroll vertical agregado en MessagesList

--- src/Components/MessagesList/MessagesList.jsx
+++ src/Components/MessagesList/MessagesList.jsx
@@ -1,10 +1,11 @@
 import Message from "../Message/Message"
+import './MessagesList.css'
 
 
 const MessagesList = ({ messages }) => {
 
   return (
-    <div>
+    <div className="messagesList">
       {messages.map((message) => {
         return <Message content={message.content} id={message.id} key={message.id} author={message.author} message={message.message} timestamp={message.timestamp} />
       })
