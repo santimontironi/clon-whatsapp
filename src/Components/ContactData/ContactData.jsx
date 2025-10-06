f42a3e183a460e2964f2b36367df6b39d97860f5
santimontironi <santiimontironi@gmail.com>
2025-10-06 19:41:04 -0300

    ContactData estilizado en mobile

--- src/Components/ContactData/ContactData.jsx
+++ src/Components/ContactData/ContactData.jsx
@@ -1,27 +1,28 @@
-import { IoArrowBackOutline } from "react-icons/io5";
-import { FaPhoneAlt } from "react-icons/fa";
-import { Link } from "react-router";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react"
 import './ContactData.css'
 
-const ContactData = ({contact}) => {
+const ContactData = ({ contact }) => {
+
+    const [close, setClose] = useState(false)
 
     return (
-        <div className="contactDataScreen">
+        <div className={`contactData ${close ? "close" : ""}`}>
 
-            <div className="contactDataScreen-container">
-                <header className="contactDataScreen-container-header">
-                    <Link to="/">
-                        <IoArrowBackOutline className="contactDataScreen-container-header-icon" color="#ffffff" size={30} />
-                    </Link>
-                    <h1 className="contactDataScreen-container-title">Info del contacto</h1>
+            <div className="contactData-container">
+                <header className="contactData-container-header">
+                    <IoMdClose className="contactData-container-header-icon" color="#ffffff" size={30} onClick={() => { setClose(!close) }} />
+                    <span className="contactData-container-header-title">Info del contacto</span>
                 </header>
-                <div className="contactDataScreen-container-content">
-                    <figure className="contactDataScreen-container-content-photo">
-                        <img className="contactDataScreen-container-content-photo-img" src={contact.photo} alt={`Foto de perfil de ${contact.name}`} />
+                <div className="contactData-container-content">
+                    <figure className="contactData-container-content-photo">
+                        <img className="contactData-container-content-photo-img" src={contact.photo} alt={`Foto de perfil de ${contact.name}`} />
                     </figure>
-                    <span className="contactDataScreen-container-content-name">Contacto: {contact.name}</span>
-                    <span className="contactDataScreen-container-content-number"><FaPhoneAlt color="#fff" size={20} /> {contact.number}</span>
-                    <span className="contactDataScreen-container-content-state">Estado: {contact.state}</span>
+                    <div className="contactData-container-content-info">
+                        <span className="contactData-container-content-name">{contact.name}</span>
+                        <span className="contactData-container-content-number">Teléfono: {contact.number}</span>
+                        <span className="contactData-container-content-state">Estado: {contact.state}</span>
+                    </div>
                 </div>
             </div>
 
